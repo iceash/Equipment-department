f456ea482775567dbb932c25781fbb19e018b35c
iceash <414798080@qq.com>
2016-06-30 01:33:56 +0800

    今日进度
    完成题库部分功能（除文件批量上传题目），以及试卷部分手工组卷、随机组卷、试卷列表功能

--- application/Exam/Controller/AdminExamController.class.php
+++ application/Exam/Controller/AdminExamController.class.php
@@ -17,7 +17,9 @@ class AdminExamController extends AdminbaseController {
 		$this->term_relationships_model = D("Portal/TermRelationships");*/
 	}
 	function index(){
-		// echo "string";
+		$exams = M("exams")->where($map)->order("id DESC")->field("id,name,type,create_time")->select();
+		$this->assign("exams",$exams);
+		// dump($exams);
 		$this->display();
 	}
 	function exam_rand(){
@@ -32,6 +34,150 @@ class AdminExamController extends AdminbaseController {
 	function editexam(){
 		$this->display();
 	}
-
+	function select_question(){
+		$map["type"] = $_GET["type"];
+		$questions = M("questions");
+		$count      = $questions->where($map)->count();
+		$Page       = new \Think\Page($count,25);
+		$show       = $Page->show();
+		$list = $questions->where($map)->order('id DESC')->limit($Page->firstRow.','.$Page->listRows)->select();
+		// $question = M("questions")->where($map)->select();
+		$this->assign("questions",$list);
+		$this->assign('page',$show);
+		$this->assign("count",$count);
+		$this->assign("type",$_GET["type"]);
+		$this->display();
+	}
+	function view_question(){
+		$type = $_POST["type"];
+		switch ($type) {
+			case '1':
+				$type_name = "single";
+				break;
+			case '2':
+				$type_name = "multi";
+				break;
+			case '3':
+				$type_name = "judge";
+				break;
+		}
+		$ids = $_POST["exam_questions"][$type_name];
+		if (count($ids) == 0) {
+			$ids[0] = 0;
+		}
+		$map["type"] = $type;
+		$map["id"] = array("in",$ids);
+		$questions = M("questions")->where($map)->select();
+		foreach ($questions as $num => $va) {
+			if ($va["type"] == "3") {
+				$questions[$num]["option"] = array("A、对","B、错");
+			}else{
+				$option = explode("|", $va["option"]);
+				for ($i=0; $i < count($option); $i++) { 
+					$option[$i] = chr(65 + $i)."、".$option[$i];
+				}
+				$questions[$num]["option"] = $option;
+			}
+		}
+		$this->assign("questions",$questions);
+		$this->display();
+	}
+	function ajaxAddExam(){
+		$data = $_POST["data"];
+		$map["name"] = $data["name"];
+		$count = M("exams")->where($map)->count();
+		if ($count > 0) {
+			$return["status"] = 0;
+			$return["info"] = "试卷名称重复";
+		}
+		$data["create_time"] = time();
+		$data["type"] = "手工组卷";
+		if (M("exams")->add($data)) {
+			$return["status"] = 1;
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "新增出错";
+		}
+		$this->ajaxReturn($return);
+	}
+	function ajaxAddRandExam(){
+		$data = $_POST["data"];
+		$map["name"] = $data["name"];
+		$count = M("exams")->where($map)->count();
+		if ($count > 0) {
+			$return["status"] = 0;
+			$return["info"] = "试卷名称重复";
+		}
+		$settings = json_decode($data["settings"]);
+		$allquestion = M("questions")->field("id,type")->select();
+		foreach ($allquestion as $key => $va) {
+			$forRand[$va["type"]][] = $va["id"];
+		}
+		$return["info"] = "";
+		$return["status"] = 1;
+		if (count($forRand["1"]) < $settings->single->number) {
+			$return["info"] .= "单选题题库数量不足  ";
+			$return["status"] = 0;
+		}
+		if (count($forRand["2"]) < $settings->multi->number) {
+			$return["info"] .= "多选题题库数量不足  ";
+			$return["status"] = 0;
+		}
+		if (count($forRand["3"]) < $settings->judge->number) {
+			$return["info"] .= "判断题题库数量不足  ";
+			$return["status"] = 0;
+		}
+		if ($return["status"] == 0) {
+			$return["info"] .= "请在题库增加题量或减少所需题量";
+			$this->ajaxReturn($return);
+		}
+		$count = 0;
+		while ($count < $settings->single->number) {
+			$i = rand(0,count($forRand["1"])-1);
+			$tmp = $forRand["1"][$i];
+			if (!in_array($tmp, $exam_questions["single"])) {
+				$exam_questions["single"][] = (int)$tmp;
+			}
+			$count = count($exam_questions["single"]);
+		}
+		$count = 0;
+		while ($count < $settings->multi->number) {
+			$i = rand(0,count($forRand["2"])-1);
+			$tmp = $forRand["2"][$i];
+			if (!in_array($tmp, $exam_questions["multi"])) {
+				$exam_questions["multi"][] = (int)$tmp;
+			}
+			$count = count($exam_questions["multi"]);
+		}
+		$count = 0;
+		while ($count < $settings->judge->number) {
+			$i = rand(0,count($forRand["3"])-1);
+			$tmp = $forRand["3"][$i];
+			if (!in_array($tmp, $exam_questions["judge"])) {
+				$exam_questions["judge"][] = (int)$tmp;
+			}
+			$count = count($exam_questions["judge"]);
+		}
+		$data["create_time"] = time();
+		$data["type"] = "随机组卷";
+		$data["exam_questions"] = json_encode($exam_questions);
+		if (M("exams")->add($data)) {
+			$return["status"] = 1;
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "新增出错";
+		}
+		$this->ajaxReturn($return);
+	}
+	function delExam(){
+		$map["id"] = $_POST["id"];
+		if (M("exams")->where($map)->delete()) {
+			$return["status"] = 1;
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "删除出错";
+		}
+		$this->ajaxReturn($return);
+	}
 	
 }
--- application/Exam/Controller/AdminQuestionController.class.php
+++ application/Exam/Controller/AdminQuestionController.class.php
@@ -17,7 +17,32 @@ class AdminQuestionController extends AdminbaseController {
 		$this->term_relationships_model = D("Portal/TermRelationships");*/
 	}
 	function index(){
-		// echo "string";
+		$questions = M('questions');
+		$map = [];
+		$count      = $questions->where($map)->count();
+		$Page       = new \Think\Page($count,25);
+		$show       = $Page->show();
+		$list = $questions->where($map)->order('id DESC')->limit($Page->firstRow.','.$Page->listRows)->select();
+		foreach ($list as $i => $va) {
+			switch ($va["type"]) {
+				case '1':
+					$list[$i]["type"] = "单选题";
+					break;
+				case '2':
+					$list[$i]["type"] = "多选题";
+					break;
+				case '3':
+					$list[$i]["type"] = "判断题";
+					break;
+				default:
+					$list[$i]["type"] = "单选题";
+					break;
+			}
+			$list[$i]["time"] = date("Y-m-d h:i:s", $va["time"]);
+		}
+		$this->assign('list',$list);
+		$this->assign('page',$show);
+		$this->assign("count",$count);
 		$this->display();
 	}
 	function addquestion()
@@ -25,10 +50,92 @@ class AdminQuestionController extends AdminbaseController {
 		$this->display();
 	}
 	function editquestion(){
+		$map["id"] = $_GET["questionid"];
+		$question = M("questions")->where($map)->find();
+		$option = explode("|", $question["option"]);
+		/*$question["option"] = array();
+		for ($i=0; $i < count($option); $i++) { 
+			$question["option"][chr(65) + $i] = $option[$i];
+		}*/
+		$question["option"] = $option;
+		$question["answer"] = explode("|", $question["answer"]);
+		$this->assign("que",$question);
 		$this->display();
 	}
 	function fileaddquestion(){
 		$this->display();
 	}
-	
+	function ajaxAddquestion(){
+		$question = $_POST['question'];
+		$data["type"] = $question["type"];
+		$data["question"] = $question["question"];
+		$data["number"] = $question["number"];
+		$data["option"] = implode("|", $question["option"]);
+		$data["describe"] = $question["describe"];
+		$data["time"] = time();
+		switch ($question["type"]) {
+			case '1':
+				$data["answer"] = $question["answer"];
+				break;
+			case '2':
+				$data["answer"] = implode("|", $question["multianswer"]);
+				break;
+			case '3':
+				$data["answer"] = $question["judgeanswer"];
+				$data["option"] = "";
+				break;
+			default:
+				$data["answer"] = $question["answer"];
+				break;
+		}
+		if (M("questions")->add($data)) {
+			$return["status"] = 1;
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "新增出错";
+		}
+		$this->ajaxReturn($return);
+	}
+	function ajaxEditquestion(){
+		$question = $_POST['question'];
+		$data["id"] = $question["id"];
+		$data["type"] = $question["type"];
+		$data["question"] = $question["question"];
+		$data["number"] = $question["number"];
+		$data["option"] = implode("|", $question["option"]);
+		$data["describe"] = $question["describe"];
+		// $data["time"] = time();
+		switch ($question["type"]) {
+			case '1':
+				$data["answer"] = $question["answer"];
+				break;
+			case '2':
+				$data["answer"] = implode("|", $question["multianswer"]);
+				break;
+			case '3':
+				$data["answer"] = $question["judgeanswer"];
+				$data["option"] = "";
+				break;
+			default:
+				$data["answer"] = $question["answer"];
+				break;
+		}
+		if (M("questions")->where($map)->save($data) == 1) {
+			$return["status"] = 1;
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "无修改";
+		}
+		$this->ajaxReturn($return);
+	}
+	function delQuestion(){
+		$map["id"] = array("in",$_POST["id"]);
+		if (M("questions")->where($map)->delete()) {
+			$return["status"] = 1;
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "删除出错";
+		}
+		$this->ajaxReturn($return);
+	}
 }
