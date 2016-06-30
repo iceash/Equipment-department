e8a0c2d618498d53426ddf67ca14b612b9ec9ee3
iceash <414798080@qq.com>
2016-06-30 22:35:13 +0800

    在线考试系统基本完工
    剩余几个遗留问题，考完试再搞

--- application/Exam/Controller/AdminHistoryController.class.php
+++ application/Exam/Controller/AdminHistoryController.class.php
@@ -9,17 +9,41 @@
 namespace Exam\Controller;
 use Common\Controller\AdminbaseController;
 class AdminHistoryController extends AdminbaseController {
-	function _initialize() {
-		//解析函数
-		/*parent::_initialize();
-		$this->posts_model = D("Portal/Posts");
-		$this->terms_model = D("Portal/Terms");
-		$this->term_relationships_model = D("Portal/TermRelationships");*/
-	}
 	function index(){
 		// echo "string";
+		$scores = M("scores");
+		$count      = $scores->where($map)->count();
+		$Page       = new \Think\Page($count,25);
+		$show       = $Page->show();
+		$list = $scores->where($map)->order('time DESC')->limit($Page->firstRow.','.$Page->listRows)->select();
+		foreach ($list as $i => $va) {
+			$list[$i]["submit_time"] = date("Y-m-d h:i:s", $va["time"]);
+		}
+		$this->assign("scores",$list);
+        $this->display();
+	}
+	function change_score(){
+		$data = $_POST["data"];
+		$map["id"] = $data["id"];
+		if (M("scores")->where($map)->save($data)) {
+			$return["status"] = 1;
+			$return["info"] = "修改成功";
+		}else{
+			$return["status"] = 0;
+			$return["info"] = "无修改";
+		}
+		$this->ajaxReturn($return);
+	}
+	function view_score(){
+		$map["id"] = $_GET["id"];
+		$scores = M("scores")->where($map)->find();
+		$result = json_decode($scores["result"]);
+		foreach ($result as $i => $va) {
+			$exam_questions[$va->type][] = $va;
+		}
+		$this->assign("exam",$scores);
+		$this->assign("exam_questions",$exam_questions);
 		$this->display();
 	}
-	
 	
 }
--- application/Exam/Controller/IndexController.class.php
+++ application/Exam/Controller/IndexController.class.php
@@ -3,11 +3,128 @@ namespace Exam\Controller;
 use Common\Controller\HomebaseController;
 
 class IndexController extends HomebaseController{
-	
+	function _initialize() {
+		//解析函数
+		// parent::_initialize();
+		$userid = $_SESSION["user"]["id"];
+		if(!$userid){
+			$this->error("您还没有登录！",U("User/login/index"));
+		}
+	}
 	function index(){
+		$exams = M("exams")->field("id,name")->select();
+		$this->assign("exams",$exams);
+		$map["userid"] = $_SESSION["user"]["id"];
+		$scores = M("scores")->where($map)->field("id,examname,time")->select();
+		$this->assign("scores",$scores);
         $this->display();
 	}
     function paper(){
-        $this->display();
+    	$map["id"] = $_GET["id"];
+		$exam = M("exams")->where($map)->find();
+		$settings = json_decode($exam["settings"]);
+		$exam_questions = json_decode($exam["exam_questions"]);
+		$ids = array_merge($exam_questions->single, $exam_questions->multi, $exam_questions->judge);
+		$map["id"] = array("in",$ids);
+		$questions = M("questions")->where($map)->select();
+		for ($i=0; $i < count($questions); $i++) { 
+			$list[$questions[$i]["id"]] = $questions[$i];
+		}
+		$this->assign("settings",$settings);
+		$this->assign("exam_questions",$exam_questions);
+		$this->assign("exam",$exam);
+		$this->assign("list",$list);
+		$this->display();
+    }
+    function submit_paper(){
+    	$answers = $_POST;
+    	$map["id"] = $answers["examid"];
+    	$exam = M("exams")->where($map)->find();
+		$settings = json_decode($exam["settings"]);
+		$exam_questions = json_decode($exam["exam_questions"]);
+		$qusetion_ids = array_merge($exam_questions->single, $exam_questions->multi, $exam_questions->judge);
+		$map["id"] = array("in",$qusetion_ids);
+		$questions = M("questions")->where($map)->select();
+		for ($i=0; $i < count($questions); $i++) { 
+			$list[$questions[$i]["id"]] = $questions[$i];
+		}
+		//answer中存放考生提交的答案
+		//list中以题目id为索引存放题目完整信息
+		//exam_questions中以题型为索引存放题目id
+		//result存放答题的完整结果信息
+		$get_score = 0;
+		for ($i=0; $i < $settings->single->number; $i++) { 
+			$id = $exam_questions->single[$i];
+			$tmp["question"] = $list[$id]["question"];
+			$tmp["type"] = "single" ;
+			$tmp["option"] = $list[$id]["option"];
+			$tmp["selected"] = $answers["radio_answer"][$id];
+			$tmp["answer"] = $list[$id]["answer"];
+			$tmp["describe"] = $list[$id]["describe"];
+			$tmp["score"] = $settings->single->score;
+			if ($tmp["selected"] == $tmp["answer"]) {
+				$tmp["bingo"] = 1;
+				$get_score += $tmp["score"];
+			}else{
+				$tmp["bingo"] = 0;
+			}
+			$result[] = $tmp;
+		}
+
+		for ($i=0; $i < $settings->multi->number; $i++) { 
+			$id = $exam_questions->multi[$i];
+			$tmp["question"] = $list[$id]["question"];
+			$tmp["type"] = "multi" ;
+			$tmp["option"] = $list[$id]["option"];
+			$tmp["selected"] = implode("|", $answers["checkbox_answer"][$id]);
+			$tmp["answer"] = $list[$id]["answer"];
+			$tmp["describe"] = $list[$id]["describe"];
+			$tmp["score"] = $settings->multi->score;
+			if ($tmp["selected"] == $tmp["answer"]) {
+				$tmp["bingo"] = 1;
+				$get_score += $tmp["score"];
+			}else{
+				$tmp["bingo"] = 0;
+			}
+			$result[] = $tmp;
+		}
+
+		for ($i=0; $i < $settings->judge->number; $i++) { 
+			$id = $exam_questions->judge[$i];
+			$tmp["question"] = $list[$id]["question"];
+			$tmp["type"] = "judge" ;
+			$tmp["option"] = $list[$id]["option"];
+			$tmp["selected"] = $answers["judge_answer"][$id];
+			$tmp["answer"] = $list[$id]["answer"];
+			$tmp["describe"] = $list[$id]["describe"];
+			$tmp["score"] = $settings->judge->score;
+			if ($tmp["selected"] == $tmp["answer"]) {
+				$tmp["bingo"] = 1;
+				$get_score += $tmp["score"];
+			}else{
+				$tmp["bingo"] = 0;
+			}
+			$result[] = $tmp;
+		}
+		$data["userid"] = $_SESSION["user"]["id"];
+		$data["username"] = $_SESSION["user"]["user_nicename"];
+		$data["examid"] = $answers["examid"];
+		$data["examname"] = $exam["name"];
+		$data["score"] = $get_score;
+		if ($get_score >= $exam["pass_line"]) {
+			$data["is_pass"] = 1;
+		}else{
+			$data["is_pass"] = 0;
+		}
+		$data["time"] = time();
+		$data["result"] = json_encode($result);
+		if (M("scores")->add($data)) {
+			$back["status"] = 1;
+			$back["info"] = "交卷成功，您的得分为".$get_score."分";
+		}else{
+			$back["status"] = 0;
+			$back["info"] = "交卷失败";
+		}
+		$this->ajaxReturn($back);
     }
 }
