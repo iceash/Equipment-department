61771679f34c8b87dc30df1309c2f5c54971f78d
zombielive <515962540@qq.com>
2016-07-10 03:14:23 +0800

    改动很多，看详情↓
    1.科室公告系统加入权限，读取session中的name作为department
    2.考勤系统改为三个页面：考勤记录显示某一时间段内考勤机记录；出勤状态显示前一天或指定一天的出勤状态及统计；设置考勤时间不变
    3.新增计算每天出勤状态的函数add_attend

--- application/Attendance/Controller/IndexController.class.php
+++ application/Attendance/Controller/IndexController.class.php
@@ -15,17 +15,44 @@ class IndexController extends AdminbaseController{
 		//$this->slidecat_model = D("Common/SlideCat");
 		
 	}
-	
-	function index(){
+	function index()
+	{
 		if(isset($_POST['department']) && $_POST['department']!=""){
 			$where['department']=$_POST['department'];
 			$this->assign("department",$_POST['department']);
 		}
-		$attend_date=date('Y-m-d');
+		$start_time=date('Y-m-d')." 00:00";
+		if(isset($_POST['start_time']) && $_POST['start_time']!=""){
+			$start_time=$_POST['start_time'];
+		}
+		$this->assign("start_time",$start_time);
+		$end_time=date('Y-m-d')." 23:59";
+		if(isset($_POST['end_time']) && $_POST['end_time']!=""){
+			$end_time=$_POST['end_time'];
+		}
+		$this->assign("end_time",$end_time);
+		$where['CHECKTIME']=array(array('gt',$start_time),array('lt',$end_time));
+		if(isset($_POST['keyword']) && $_POST['keyword']!=""){
+			$keyword=$_POST['keyword'];
+			$where['_string']='(name like "%'.$keyword.'%")OR(badge_number like "%'.$keyword.'%")';
+			$this->assign("keyword",$keyword);
+		}
+		$attend=M('checkinout')->where($where)->order("CHECKTIME ASC")->select();
+		$this->assign('attend',$attend);
+		$department_list=M('department')->Field('department_name')->select();
+		$this->assign("dep_list",$department_list);
+		$this->display();
+	}
+	function attend(){
+		if(isset($_POST['department']) && $_POST['department']!=""){
+			$where['department']=$_POST['department'];
+			$this->assign("department",$_POST['department']);
+		}
+		$attend_date=date('Y-m-d',strtotime("-1 day"));
 		if(isset($_POST['attend_date']) && $_POST['attend_date']!=""){
-			$where['attend_date']=$_POST['attend_date'];
 			$attend_date=$_POST['attend_date'];
 		}
+		$where['attend_date']=$attend_date;
 		$this->assign("attend_date",$attend_date);
 		$ijo_count=$this->attend_model->where($where)->where("status='异常'")->count();
 		$this->assign("ijo_count",$ijo_count);
@@ -36,7 +63,7 @@ class IndexController extends AdminbaseController{
 		}
 		$attend=$this->attend_model->where($where)->order("name ASC")->select();
 		$this->assign('attend',$attend);
-		$department_list=$this->attend_model->group('department')->Field('department')->select();
+		$department_list=M('department')->Field('department_name')->select();
 		$this->assign("dep_list",$department_list);
 		$this->display();
 	}
@@ -84,6 +111,63 @@ class IndexController extends AdminbaseController{
 			} else {
 				$this->error("修改失败！");
 			}
+		}
+	}
+	function add_attend(){
+		$users=M('users')->Field('user_nicename,department_name')->select();
+		$Attend=M('attend');
+		$today=date('Y-m-d');
+		$week=date('w');
+		if($week=='0'||$week=='6'){
+			$is_weekend=1;//测试时修改这里
+		}else{
+			$is_weekend=0;
+		}
+		$wd['key']='on_time_rule';
+		$on=M('dictionary')->where($wd)->getField('value');
+		$wd['key']='off_time_rule';
+		$off=M('dictionary')->where($wd)->getField('value');
+		for ($i=0; $i < count($users); $i++) { 
+			$data['name']=$users[$i]['user_nicename'];
+			$data['department']=$users[$i]['department_name'];
+			$data['attend_date']=$today;
+			$Attend->add($data);
+			$map['NAME']=$users[$i]['user_nicename'];
+			$map['CHECKTIME']=array(array('gt',$today." 00:00"),array('lt',$today." 23:59"));
+			$check_time=M('checkinout')->where($map)->Field('CHECKTIME')->order('CHECKTIME')->select();
+			if(count($check_time)==2){
+				$data2['on_time']=date('H:i:s', strtotime($check_time[0]['checktime']));
+				$data2['off_time']=date('H:i:s', strtotime($check_time[1]['checktime']));
+			}elseif (count($check_time)==1) {
+				$data2['on_time']=date('H:i:s', strtotime($check_time[0]['checktime']));
+			}
+			if($is_weekend){
+				$data2['status']='正常';
+				$data2['content']='周末';
+			}else{
+				if(count($check_time)==0){
+					$data2['status']='异常';
+					$data2['content']='缺勤';
+				}elseif (count($check_time)==1) {
+					$data2['status']='异常';
+					$data2['content']='记录缺失';
+				}else{
+					$on_cha=strtotime($check_time[0]['checktime'])-strtotime($today." ".$on);
+					$off_cha=strtotime($check_time[1]['checktime'])-strtotime($today." ".$off);
+					$data2['status']='正常';
+					$data2['content']="";
+					if($on_cha>0){
+						$data2['status']='异常';
+						$data2['content']=$data2['content'].'迟到';
+					}
+					if($off_cha<0){
+						$data2['status']='异常';
+						$data2['content']=$data2['content'].'早退';
+					}
+				}
+			}
+			$Attend->where($data)->save($data2);
+			$data2='';
 		}
 	}
 }
--- application/Gonggao/Controller/IndexController.class.php
+++ application/Gonggao/Controller/IndexController.class.php
@@ -5,9 +5,10 @@ use Common\Controller\HomebaseController;
 class IndexController extends HomebaseController{
 	
 	function index(){
-		$screen=D("Common/NoticeScreen")->where("screen_status=1")->order("listorder ASC")->select();
+		$department="'".$_GET['department']."'";
+		$screen=D("Common/NoticeScreen")->where("screen_status=1 and department=".$department)->order("listorder ASC")->select();
 		$this->assign('screen',$screen);
-		$interval=D("Common/Notice")->where("department='测试'")->getField("interval");
+		$interval=D("Common/Notice")->where("department=".$department)->getField("interval");
 		$this->assign('interval',$interval);
 		$this->display();
 	}
