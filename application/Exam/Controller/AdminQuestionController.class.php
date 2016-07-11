4730f168ad2070a308e8a83543dff0b9f9f4971c
iceash <414798080@qq.com>
2016-07-12 00:54:09 +0800

    文件批量上传题目完成

--- application/Exam/Controller/AdminQuestionController.class.php
+++ application/Exam/Controller/AdminQuestionController.class.php
@@ -65,6 +65,66 @@ class AdminQuestionController extends AdminbaseController {
 	function fileaddquestion(){
 		$this->display();
 	}
+	function uploadfile(){
+		$filename = $_FILES['file']['tmp_name']; 
+		$realname = $_FILES["file"]["name"];
+		// 获得文件扩展名
+		$temp_arr = explode(".", $realname);
+		$file_ext = array_pop($temp_arr);
+		$file_ext = trim($file_ext);
+		$file_ext = strtolower($file_ext); 
+		$filelocal = dirname(__FILE__) . '\..\..\..';
+		$new_file_name = date("YmdHis") . '_' . rand(10000, 99999) . '.' . $file_ext; 
+		$total = $filelocal . "\upload/" . $new_file_name;
+	    move_uploaded_file($_FILES["file"]["tmp_name"], $total);
+	    if (empty ($total)) {
+	        echo '请选择要导入的CSV文件！'; 
+	        exit; 
+	    } 
+	    $handle = fopen($total, 'r'); 
+	    $result = $this->input_csv($handle); //解析csv 
+	    $len_result = count($result); 
+	    if($len_result==0){ 
+	    	$this->error("没有任何数据！");
+	    } 
+	    for ($i = 1; $i < $len_result; $i++) { //循环获取各字段值 
+	    	$tmpi = $i + 2;
+	    	$type = $result[$i][0];
+	    	switch ($type) {
+	    		case '选择题':
+	    			$data[$i-1]["type"] = 1;
+	    			break;
+	    		case '多选题':
+	    			$data[$i-1]["type"] = 2;
+	    			break;
+	    		case '判断题':
+	    			$data[$i-1]["type"] = 3;
+	    			break;
+	    		default:
+			    	$this->error("第$tmp行题型输入错误");
+	    			break;
+	    	}
+	        $data[$i-1]["question"] = $result[$i][1];
+	        $data[$i-1]["answer"] = $result[$i][3];
+	        if ($data[$i-1]["question"] == "" || $data[$i-1]["answer"] == "") {
+			    	$this->error("第$tmp行题目不完整错误");
+	        }
+	        $data[$i-1]["describe"] = $result[$i][4];
+	        $data[$i-1]["time"] = time();
+	    	if ($type == "判断题") {
+		        $data[$i-1]["option"] = "";
+	    	}else{
+		        $data[$i-1]["option"] = $result[$i][2];
+	    	}
+	        $options = explode("|", $data[$i-1]["option"]);
+	        $data[$i-1]["number"] = count($options);
+	    } 
+	    	M("questions")->addAll($data);
+	    	$this->success("上传成功","index",0.5);
+
+	    // $data_values = substr($data_values,0,-1); //去掉最后一个逗号
+		return 0;
+	}
 	function ajaxAddquestion(){
 		$question = $_POST['question'];
 		$data["type"] = $question["type"];
@@ -138,4 +198,17 @@ class AdminQuestionController extends AdminbaseController {
 		}
 		$this->ajaxReturn($return);
 	}
+	function input_csv($handle) { 
+	    $out = array (); 
+	    $n = 0; 
+	    while ($data = fgetcsv($handle)) { 
+	    	$data = eval('return '.iconv('gbk','utf-8',var_export($data,true)).';');
+	        $num = count($data); 
+	        for ($i = 0; $i < $num; $i++) { 
+	            $out[$n][$i] = $data[$i]; 
+	        } 
+	        $n++; 
+	    } 
+	    return $out; 
+	} 
 }
